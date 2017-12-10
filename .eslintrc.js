7996c67bfe6c09a97f815f5c07e11089fc57a884
Luchansky Oleg <luchansodev@gmail.com>
2017-12-11 00:19:16 +0300

    Add lints

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,23 @@
+const path = require('path');
+
 module.exports = {
-    "extends": "airbnb",
-    "parser": "babel-eslint",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ],
-    "env": {
-        "browser": true,
-        "node": true,
-        "jest": true,
-    }
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "settings": {
+    "import/resolver": {
+      node: {
+        paths: [path.resolve(__dirname, './src')],
+      },
+    },
+  },
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "env": {
+    "browser": true,
+    "node": true,
+    "jest": true,
+  }
 };
