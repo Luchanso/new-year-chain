90e589d6030a54273c06e37286a4477d017014dc
Oleg Loutchansky <luchansodev@gmail.com>
2018-12-06 01:07:51 +0300

    remove old dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import Loadable from 'react-loadable';
 import loading from 'components/Loading';
-import store from 'store';
 
 const AsyncMain = Loadable({
   loader: () => new Promise(res => setTimeout(() => res(import('pages/Main')), 500)),
@@ -11,11 +9,9 @@ const AsyncMain = Loadable({
 });
 
 const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Route path="/" exact component={AsyncMain} />
-    </Router>
-  </Provider>
+  <Router>
+    <Route path="/" exact component={AsyncMain} />
+  </Router>
 );
 
 export default App;
