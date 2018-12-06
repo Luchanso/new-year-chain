a47336b02a32c1f36fd686f03843ddbc4af02be5
Oleg Loutchansky <luchansodev@gmail.com>
2018-12-06 23:27:50 +0300

    Add some stiles

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -28,13 +28,59 @@ const Root = styled.div`
   background: linear-gradient(to right, #6441a5, #2a0845);
   display: flex;
   background-color: white;
-  min-width: 100vw;
   min-height: 96vh;
+  padding: 0 64px;
+`;
+
+const Column = styled.div`
+  width: 50%;
+`;
+
+const AlignAction = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: calc(100% - 64px);
+`;
+
+const Description = styled.h1`
+  font-size: 64px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Open Sans', 'Helvetica Neue', sans-serif;
+  color: #fff;
+  font-weight: 200;
+`;
+
+const MessageBox = styled.textarea`
+  width: 100%;
+  border-radius: 8px;
+  border-color: white;
+  resize: none;
+  padding: 8px;
+  outline: none;
+  overflow-y: hidden;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 14px;
+  box-sizing: border-box;
 `;
 
 const Main = () => (
   <Fragment>
-    <Root>{/* <ExchangeRates /> */}</Root>
+    <Root>
+      <Column>
+        <AlignAction>
+          <Description>
+            Отправь новогоднее обещание в
+            {' '}
+            <b>Blockchain!</b>
+          </Description>
+          <MessageBox>В 2018 году обещаю </MessageBox>
+        </AlignAction>
+      </Column>
+      <Column />
+      {/* <ExchangeRates /> */}
+    </Root>
     <Header>
       <Mask />
     </Header>
